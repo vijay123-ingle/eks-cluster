1ac7d54056933cf1e6cfd29221defd021bd99053
vijay123-ingle <71634083+vijay123-ingle@users.noreply.github.com>
2025-09-29 19:52:04 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,34 +1,47 @@
 module "eks" {
+
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "19.15.1"
 
-  name               = "${var.name}-cluster"
-  kubernetes_version = "1.31"
+  cluster_name                   = "${var.name}-cluster"
+  cluster_endpoint_public_access = true
 
-  # Optional
-  endpoint_public_access = true
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_groups = {
-    my-eks-ng = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      instance_types = var.instance_types
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+
+  eks_managed_node_group_defaults = {
+
+    instance_types = var.instance_types
+
+    attach_cluster_primary_security_group = true
+
   }
+  eks_managed_node_groups = {
+
+    my-eks-ng = {
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = var.instance_types
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = "${var.name}-cluster"
 }
